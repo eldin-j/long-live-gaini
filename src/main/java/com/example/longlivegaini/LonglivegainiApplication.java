a7d8ea5104051162c3baef9a9f206adc84584ec4
Eldin <eldin.jiyembayev@gmail.com>
2025-02-04 18:46:45 +0500

    initial

---
+++ src/main/java/com/example/longlivegaini/LonglivegainiApplication.java
@@ -0,0 +1,13 @@
+package com.example.longlivegaini;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LonglivegainiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LonglivegainiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/longlivegaini/LonglivegainiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.longlivegaini;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LonglivegainiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
